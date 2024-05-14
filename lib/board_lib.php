d52351cb47a61c27b30dfb4133532f6164e59a9a
June <sungjune1993@gmail.com>
2024-05-15 01:04:33 +0900

    [4주차]-6 게시판 페이징 구현
    페이징 기능 구현

--- lib/board_lib.php
+++ lib/board_lib.php
@@ -11,10 +11,25 @@ if (isset($_POST["type"])) {
     if (strcmp($_POST["type"], "list") == 0) {
         echo get_board_list();
     }
+    else if (strcmp($_POST["type"], "paging") == 0 && isset($_POST["page"]) && isset($_POST["cnt"])) {
+        $cnt = $_POST["cnt"];
+        $index = ($_POST["page"]-1) * $cnt;
+        // echo $index;
+        echo get_board_list_paging($index, $cnt);
+    }
 }
 
+//전체 게시글 갯수
+function get_board_list_cnt()
+{
+    $conn = connect_to_db();
+    $sql = "select count(*) from my_board";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    return $row[0]["count(*)"];
+}
 
-
+// 전체 게시글 목록
 function get_board_list()
 {
     $conn = connect_to_db();
@@ -24,6 +39,15 @@ function get_board_list()
     return json_encode($row);
 }
 
+// 게시글 목록 with 페이징
+function get_board_list_paging($index, $count)
+{
+    $conn = connect_to_db();
+    $sql = "select * from my_board as b left join (select user, id as _id from my_user) u on b.user_id = u._id limit $index, $count";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    return json_encode($row);
+}
 
 
 function register_board($title, $content)
